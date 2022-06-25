1c900a8a239af7782d1667503988b8622cd2c4f0
wackmiller <jakemolinar@gmail.com>
2022-06-24 20:58:37 -0600

    db

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -69,22 +69,22 @@ WSGI_APPLICATION = 'chatapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config("NAME"),                      
-        'USER': config("USER"),
-        'PASSWORD': config("PASSWORD"),
-        'HOST': config("HOST"),
-        'PORT': config("PORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': config("NAME"),                      
+#         'USER': config("USER"),
+#         'PASSWORD': config("PASSWORD"),
+#         'HOST': config("HOST"),
+#         'PORT': config("PORT"),
+#     }
+# }
 
 
 # Password validation
